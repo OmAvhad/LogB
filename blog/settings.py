75bfe963240ec039a45f82af39288d5277fba6e3
Om Avhad <89376666+OmAvhad@users.noreply.github.com>
2023-01-03 23:10:44 +0530

    Revert "updated static files name"

--- blog/settings.py
+++ blog/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'statics/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'statics'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
