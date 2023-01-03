11528fceef048e8b6f1b3c9636990f71f4f0949f
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-01-04 01:46:31 +0530

    Login page bug fixed

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = "main"
 urlpatterns = [
     path('',views.home,name="home"),
     path('login/',views.login_template,name="login"),
-    path('register/',views.register_template,name="login"),
+    path('register/',views.register_template,name="register"),
     path('api/signin/',views.login_api,name="signin"),
     path('api/signup/',views.register_api,name="signup")
 ]
