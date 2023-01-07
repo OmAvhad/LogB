aa7fc96cde405619495e28bc3e97b3ab3cf4ccc4
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-01-07 17:20:25 +0530

    Created write.html file to write articles

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('login/',views.login_template,name="login"),
     path('register/',views.register_template,name="register"),
+    
     path('api/signin/',views.login_api,name="signin"),
     path('api/signup/',views.register_api,name="signup")
 ]
