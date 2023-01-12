18ed1fff0bed11e91168961de166d4ce28833022
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2023-01-12 16:31:04 +0530

    made changes for vercel

--- logb/settings.py
+++ logb/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'logb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
